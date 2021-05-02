ee86759f4307c76d106db27db44d7271b07871bf
jigglu <73725809+jigglu@users.noreply.github.com>
2021-05-02 20:30:02 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,17 @@
 import React, { FC } from 'react'
-
 import { About } from '../components/home/About'
-import { Partners } from '../components/home/Partners'
 import { Intro } from '../components/home/Intro'
 import { Security } from '../components/home/Security'
 import { Governance } from '../components/home/Governance'
-import { Growth } from '../components/home/Growth'
-import { Backers } from '../components/home/Backers'
-import { DataProvider } from '../components/home/DataProvider'
+
 
 export const Home: FC = () => {
   return (
-    <DataProvider>
-      <Intro />
+    
+   <Intro>
       <About />
-      <Growth />
       <Security />
-      <Partners />
-      <Governance />
-      <Backers />
-    </DataProvider>
+      <Governance />          
+    <Intro />
   )
 }
